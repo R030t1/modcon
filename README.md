6094d559e6c424f6cc92db9bf862aa3cfb24529f
Sid Spry <R030t1@gmail.com>
2017-05-02 12:03:38 -0500

    Add basic modbus code.

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Command line interface for modbus communication.
 ## Build Instructions
 
 * Run `make`.
+
+## TODO
+
+* Use `gengetopt` to generate `getopt` code.
+* Implement all instruction types.
+	- Register and bit read/write first.
--- modcon.c
+++ modcon.c
@@ -2,7 +2,39 @@
 #include <stdlib.h>
 #include <modbus/modbus.h>
 
+#define handle_error(m) \
+	do { perror(m); exit(EXIT_FAILURE); } while (0);
+
 int
 main(int argc, char *argv[])
 {
+	modbus_t *mb;
+	uint16_t reg[32],
+			 loc = 0;
+			 val = 0;
+	
+	mb = modbus_new_tcp(argv[1], MODBUS_TCP_DEFAULT_PORT);
+	if (!mb)
+		handle_error("modbus_new_tcp");
+
+	if (modbus_connect(mb) == -1) {
+		modbus_free(mb);
+		handle_error("modbus_connect");
+	}
+
+	// TODO: Parse arguments for the various instruction types.
+	loc = atoi(argv[2]);
+	val = atoi(argv[3]);
+	//if (modbus_write_register(mb, 0, val) == -1) {
+	if (modbus_write_bit(mb, 200, val) == -1) {
+		modbus_close(mb);
+		modbus_free(mb);
+		handle_error("modbus_write_bit");
+	}
+
+	modbus_close(mb);
+	modbus_free(mb);
+
+	return EXIT_SUCCESS;
+}
 }
